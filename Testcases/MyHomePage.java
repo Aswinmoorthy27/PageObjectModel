2b97fb266534e1ac379c0fd35b6832c9afcbce6e
Aswinmoorthy27 <137639736+Aswinmoorthy27@users.noreply.github.com>
2023-08-08 21:52:33 +0530

    Update MyHomePage.java

--- Testcases/MyHomePage.java
+++ Testcases/MyHomePage.java
@@ -6,28 +6,18 @@ import org.testng.annotations.Test;
 
 import BasePackage.Base;
 
-public class MyHomePage extends Base{
+public class MyHomePage extends Base {
 
-	
-	public MyHomePage myhomeverify() 
-	{
-	WebElement Myhome=	driver.findElement(By.xpath("//a[text()='My Home']"));
-	String MyHomeVerify=Myhome.getText();
-	System.out.println(""+MyHomeVerify);
-	
-	if(MyHomeVerify.equals(MyHomeVerify)) 
-	{
-		System.out.println("MyHome Is Verified");
-	}
-	return this;
-	
-	
-		
-	}
-	
+	public MyLeadsPage myhomeverify() {
+		WebElement Myhome = driver.findElement(By.xpath("//a[text()='My Home']"));
+		String MyHomeVerify = Myhome.getText();
+		System.out.println("" + MyHomeVerify);
 
-	public MyLeadsPage Clickleads() {
-		driver.findElement(By.linkText("Leads")).click();
-        return new MyLeadsPage();
+		if (MyHomeVerify.equals(MyHomeVerify)) {
+			System.out.println("MyHome Is Verified");
+		}
+		return new MyLeadsPage();
+
 	}
+
 }
