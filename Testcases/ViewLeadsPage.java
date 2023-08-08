1382bbdaecb4a5235d332f5187d722bf80930d2d
Aswinmoorthy27 <137639736+Aswinmoorthy27@users.noreply.github.com>
2023-08-08 21:44:28 +0530

    Update ViewLeadsPage.java

--- Testcases/ViewLeadsPage.java
+++ Testcases/ViewLeadsPage.java
@@ -1,5 +1,27 @@
 package Testcases;
 
-public class ViewLeadsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import BasePackage.Base;
+
+public class ViewLeadsPage extends Base {
+
+	public ViewLeadsPage verifyleadpage() {
+		String Currentpage = driver.getTitle();
+		System.out.println(Currentpage);
+        return this;
+	}
+
+	public ViewLeadsPage VerifyFirstname() {
+		WebElement Firstname = driver.findElement(By.xpath("(//span[text()='First name']//following::td)[1]"));
+		String Fname = Firstname.getText();
+		System.out.println(Fname);
+
+		if (Fname.equalsIgnoreCase("Subraja")) {
+			System.out.println("Verified");
+		}
+
+        return new ViewLeadsPage();
+	}
 }
